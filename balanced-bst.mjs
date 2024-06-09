d372de2da789c40cb3ddf0d2c49124215be3bf2b
Isutomu <isutomu@gmail.com>
2024-06-09 13:49:44 -0300

    Make buildTree function on class Tree

--- balanced-bst.mjs
+++ balanced-bst.mjs
@@ -1,5 +1,28 @@
+import { sort } from "./sort.mjs";
+
 function Node(data) {
   let right = null;
   let left = null;
   return { data, left, right };
+}
+
+function buildTree(arr, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return null;
+  }
+
+  const midIndex = Math.floor((start + end) / 2);
+  const root = Node(arr[midIndex]);
+
+  root.left = buildTree(arr, start, midIndex - 1);
+  root.right = buildTree(arr, midIndex + 1, end);
+
+  return root;
+}
+
+class Tree {
+  _root;
+  constructor(arr) {
+    this._root = buildTree(sort(arr, true));
+  }
 }
