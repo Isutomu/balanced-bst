577cfc7c0bd9af1e1b419d5a012aced23d532cde
Isutomu <isutomu@gmail.com>
2024-06-10 22:36:23 -0300

    Place buildTree function inside Tree class

--- balanced-bst.mjs
+++ balanced-bst.mjs
@@ -6,24 +6,24 @@ function Node(data) {
   return { data, left, right };
 }
 
-function buildTree(arr, start = 0, end = arr.length - 1) {
-  if (start > end) {
-    return null;
-  }
-
-  const midIndex = Math.floor((start + end) / 2);
-  const root = Node(arr[midIndex]);
-
-  root.left = buildTree(arr, start, midIndex - 1);
-  root.right = buildTree(arr, midIndex + 1, end);
-
-  return root;
-}
-
 export default class Tree {
   _root;
   constructor(arr) {
-    this._root = buildTree(sort(arr, true));
+    this._root = this.buildTree(sort(arr, true));
+  }
+
+  buildTree(arr, start = 0, end = arr.length - 1) {
+    if (start > end) {
+      return null;
+    }
+
+    const midIndex = Math.floor((start + end) / 2);
+    const root = Node(arr[midIndex]);
+
+    root.left = this.buildTree(arr, start, midIndex - 1);
+    root.right = this.buildTree(arr, midIndex + 1, end);
+
+    return root;
   }
 
   insert(value, node = this._root) {
